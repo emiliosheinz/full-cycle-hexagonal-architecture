ea05f7c4e078559566b684bb464f8443b511a06e
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-09-29 13:01:52 -0300

    feat: db adabpter save and tests

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -10,6 +10,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
@@ -26,4 +30,48 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 		return nil, err
 	}
 	return &product, nil
+}
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("select count(*) from products where id=?", product.GetId()).Scan(&rows)
+	if rows == 0 {
+		return p.create(product)
+	}
+	return p.update(product)
+}
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("insert into products(id, name, price, status) values(?, ?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(
+		product.GetId(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec(
+		"update products set name=?, price=?, status=? where id=?",
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+		product.GetId(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
 }
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,8 +1,13 @@
-package db
+package db_test
 
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/emiliosheinz/exagonal-architecture/adapters/db"
+	"github.com/emiliosheinz/exagonal-architecture/application"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -18,7 +23,7 @@ func createTable(db *sql.DB) {
     "id" string, 
     "name" string,
     "price" float,
-    "status" string,
+    "status" string
   )`
 	stmt, err := db.Prepare(table)
 	if err != nil {
@@ -39,4 +44,39 @@ func createProduct(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setup()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("c7bc89c9-3866-4427-9e05-6e92ae9d3ed3")
+	require.Nil(t, err)
+	require.Equal(t, "Test Product", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestSaveProductDb_Save(t *testing.T) {
+	setup()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Test Product"
+	product.Price = 100.0
+
+	result, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.GetName(), result.GetName())
+	require.Equal(t, product.GetPrice(), result.GetPrice())
+	require.Equal(t, product.GetStatus(), result.GetStatus())
+
+	product.Status = "enabled"
+
+	result, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.GetName(), result.GetName())
+	require.Equal(t, product.GetPrice(), result.GetPrice())
+	require.Equal(t, product.GetStatus(), result.GetStatus())
 }
