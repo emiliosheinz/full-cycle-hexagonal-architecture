1443a0169ee329b43b968a3d9aa644f6cfc83baa
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-09-29 15:45:26 -0300

    feat: new product service func

--- application/product_service.go
+++ application/product_service.go
@@ -12,6 +12,10 @@ func (s *ProductService) Get(id string) (ProductInterface, error) {
 	return product, nil
 }
 
+func NewProductService(persistence ProductPersistanceInterface) *ProductService {
+	return &ProductService{Persistence: persistence}
+}
+
 func (s *ProductService) Create(name string, price float64) (ProductInterface, error) {
 	product := NewProduct()
 	product.Name = name
